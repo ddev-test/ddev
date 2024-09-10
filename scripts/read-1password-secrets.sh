f4d42d5b316b4cfe0abfbe2cff3b93cfb78d8d18
Randy Fay <randy@randyfay.com>
2024-09-10 14:52:49 -0600

    Import the env vars

--- scripts/read-1password-secrets.sh
+++ scripts/read-1password-secrets.sh
@@ -4,8 +4,10 @@
 
 for item in DDEV_ACQUIA_API_KEY DDEV_ACQUIA_API_SECRET DDEV_PANTHEON_API_TOKEN DDEV_PLATFORM_API_TOKEN DDEV_UPSUN_API_TOKEN; do
   printf "export ${item}=$(op item get ${item} --field=credential --reveal)\n"
-done
+done >/tmp/1penv.sh
 
 for item in DDEV_ACQUIA_SSH_KEY DDEV_LAGOON_SSH_KEY DDEV_PANTHEON_SSH_KEY; do
   printf "export ${item}=$(op item get --field='private key' --reveal ${item})\n"
-done
+done >> /tmp/1penv.sh
+
+echo "You can now 'source /tmp/1penv.sh' to get the new environment variables"
